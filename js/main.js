204cb097defadc6a477ffc915be309761a029541
Muzammil React(Developer) <79418859+muzammil7912@users.noreply.github.com>
2023-11-07 10:24:05 +0500

    mu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
-let cursor = document.querySelector('.cursor');
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.clientX+ 'px';
-    cursor.style.top = e.clientY + 'px';
+
+$(document).on('mousemove', (e) => {
+    $('.cursor').css({
+        'left': e.clientX + 'px',
+        'top': e.clientY + 'px'
+    });
+});
+
+$(".banner h1").on('mouseenter', (e) => {
+    $(".cursor").addClass("active")
+})
+$(".header__logo").on('mouseenter', (e) => {
+    $(".cursor").addClass("active")
+})
+$(".header__center").on('mouseenter', (e) => {
+    $(".cursor").addClass("active")
+})
+$(".header__right").on('mouseenter', (e) => {
+    $(".cursor").addClass("active")
+})
+$(".ProductSection li").on('mouseenter', (e) => {
+    $(".cursor").addClass("active")
+})
+$(".banner h1").on('mouseleave', (e) => {
+    $(".cursor").removeClass("active")
+})
+$(".header__logo").on('mouseleave', (e) => {
+    $(".cursor").removeClass("active")
+})
+$(".header__center").on('mouseleave', (e) => {
+    $(".cursor").removeClass("active")
+})
+$(".header__right").on('mouseleave', (e) => {
+    $(".cursor").removeClass("active")
+})
+$(".ProductSection li").on('mouseleave', (e) => {
+    $(".cursor").removeClass("active")
 })
